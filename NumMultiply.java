508e318f7eed4ebb8340ae20d12039e932a411d3
mYang25 <90435527+mYang25@users.noreply.github.com>
2022-02-04 14:38:08 -0800

    Insert JUnit testing files and edit code

--- NumMultiply.java
+++ NumMultiply.java
@@ -1,1 +1,7 @@
 // Will edit in VSCode
+
+public class NumMultiply{
+    public static int multiply(int a, int b){
+        return a + b;
+    }
+}
---
+++ NumMultiplyTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumMultiplyTest {
+    @Test
+    public void addition() {
+        assertEquals(12, NumMultiply.multiply(3, 4));
+    }
+}
